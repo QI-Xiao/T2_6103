cd565790de80f00c1c82ea3aa506f468ac29b789
Akshat Saini <akshatsaini1@gmail.com>
2022-04-16 14:54:10 -0400

    Created the py file stroke.py and loaded the data set.

---
+++ stroke.py
@@ -0,0 +1,11 @@
+#%%
+# Importing packages
+import os
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns 
+#%%
+# Loading the data set
+df = pd.read_csv('stroke dataset.csv')
+# %%

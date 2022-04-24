6a0b97e204d24a64dd86f3f2edffd7e11011a7da
Akshat Saini <akshatsaini1@gmail.com>
2022-04-24 15:20:24 -0400

    Update stroke.py

--- stroke.py
+++ stroke.py
@@ -1,30 +1,34 @@
-#%%[Markdown]
+# %%[Markdown]
 
 # T2 Project - Stroke Detection
-#%%
+# %%
 # Importing packages
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from imblearn.over_sampling import SMOTE
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sn 
+import seaborn as sn
 
-#%%
+# %%
 # Loading the data set
 df = pd.read_csv('stroke dataset.csv')
 # %%
 df.info()
 df.head()
 
-# Starting inital EDA 
-# -'id' column is randomly generated int digits, hence not required.    
-# -Converting 'age' column to type int64. 
+# Starting inital EDA
+# -'id' column is randomly generated int digits, hence not required.
+# -Converting 'age' column to type int64.
 # -'avg_glucose_level' is of type float64.
-# -'Stroke' column is the target variable or y. 
+# -'Stroke' column is the target variable or y.
 # -Except for 'bmi' and 'avg_glucose_level', all other columns are categorical in nature.
 
 # %%
-df.drop('id', axis = 1, inplace = True)
+df.drop('id', axis=1, inplace=True)
 df[df['gender'] == 'Other']
 df = df.drop(3116)
 # %%
@@ -40,17 +44,22 @@ Now converting categorical columns to ordinal/numeric values.
     smoking_status     |never_smoked|unknown|formerly_smoked|smokes --> 0|1|2|3
  
 '''
-#%%
-df['gender'].replace(to_replace=['Male', 'Female'], value=['1', '0'], inplace=True)
-df['ever_married'].replace(to_replace=['Yes', 'No'], value=['1', '0'], inplace=True)
-df['work_type'].replace(to_replace=['Private', 'Self-employed', 'children', 'Govt_job', 'Never_worked'], value=['0', '1', '2', '3', '4'], inplace=True)
-df['Residence_type'].replace(to_replace=['Urban', 'Rural'], value=['1', '0'], inplace=True)
-df['smoking_status'].replace(to_replace=['never smoked', 'Unknown', 'formerly smoked', 'smokes'], value=['0', '1', '2', '3'], inplace=True)
+# %%
+df['gender'].replace(to_replace=['Male', 'Female'],
+                     value=['1', '0'], inplace=True)
+df['ever_married'].replace(to_replace=['Yes', 'No'],
+                           value=['1', '0'], inplace=True)
+df['work_type'].replace(to_replace=['Private', 'Self-employed', 'children',
+                        'Govt_job', 'Never_worked'], value=['0', '1', '2', '3', '4'], inplace=True)
+df['Residence_type'].replace(to_replace=['Urban', 'Rural'], value=[
+                             '1', '0'], inplace=True)
+df['smoking_status'].replace(to_replace=['never smoked', 'Unknown',
+                             'formerly smoked', 'smokes'], value=['0', '1', '2', '3'], inplace=True)
 
 df.head()
 
-#%%
-# BMI column has 201 NaN values. Dropping rows with NaN values. 
+# %%
+# BMI column has 201 NaN values. Dropping rows with NaN values.
 # drop all rows that have any NaN values
 df = df.dropna()
 
@@ -58,7 +67,7 @@ df = df.dropna()
 df = df.reset_index(drop=True)
 
 df.shape
-#%%
+# %%
 value_count_stroke = df['stroke'].value_counts()
 print(value_count_stroke)
 
@@ -66,10 +75,10 @@ print(value_count_stroke)
 # 1     209
 # Name: stroke, dtype: int64
 
-# From initial analysis, the dataset seems to be highly unbalanced. 
-# There are 4699 cases without a stroke and 209 cases with a stroke among the participant list. 
-#%%
-# Further subdiving the dataset into male and female sets. 
+# From initial analysis, the dataset seems to be highly unbalanced.
+# There are 4699 cases without a stroke and 209 cases with a stroke among the participant list.
+# %%
+# Further subdiving the dataset into male and female sets.
 
 grouped = df.groupby(df['gender'])
 male_stroke = grouped.get_group('1')
@@ -79,36 +88,35 @@ print(male_stroke['stroke'].value_counts())
 # 0    1922
 # 1     89
 # Name: stroke, dtype: int64
-# Extremely unbalanced set. 108 men with stroke and 2007 without stroke. 
+# Extremely unbalanced set. 108 men with stroke and 2007 without stroke.
 print(female_stroke['stroke'].value_counts())
 # 0    2777
 # 1     120
 # Name: stroke, dtype: int64
-# Extremely unbalanced set. 141 women with stroke and 2853 without stroke. 
+# Extremely unbalanced set. 141 women with stroke and 2853 without stroke.
 
 
 # %%
-# Above analysis suggests that the data set is highly unbalanced. 
-# This will require balancing the dataset (target variable) before any furthur tests or analysis can be conducted! 
+# Above analysis suggests that the data set is highly unbalanced.
+# This will require balancing the dataset (target variable) before any furthur tests or analysis can be conducted!
 
-# Balancing the data set using SMOTE: Synthetic Minority Oversampling Technique. Using this method, as the name suggests, the minority target variable is oversampled using random values. The technique uses the concept of K-NN or K neareast neighbors to intelligently generate synthetic data which resembles the values or shape of the outnumbered data instead of directly copying or reusing pre-existing values. 
+# Balancing the data set using SMOTE: Synthetic Minority Oversampling Technique. Using this method, as the name suggests, the minority target variable is oversampled using random values. The technique uses the concept of K-NN or K neareast neighbors to intelligently generate synthetic data which resembles the values or shape of the outnumbered data instead of directly copying or reusing pre-existing values.
 # For more info: https://github.com/scikit-learn-contrib/imbalanced-learn
 
 
+# separating the target variable from the main data set.
 
-# separating the target variable from the main data set. 
-
-X = df.drop('stroke', axis = 'columns') # regressor data set
-y = df['stroke'] # target variable data set
+X = df.drop('stroke', axis='columns')  # regressor data set
+y = df['stroke']  # target variable data set
 
 print(y.value_counts())
 # 0    4699
 # 1     209
 # Name: stroke, dtype: int64
 
-#%%
-# Converting column data type to int64 so ordinal values remain as int and not get float values when SMOTE is being performed as the process will generate synthetic values based on KNN algorithm. 
-# For eg: We have to make sure that column values stay IN [1,0] and not something like 0.55 when synthetic values are being set up. 
+# %%
+# Converting column data type to int64 so ordinal values remain as int and not get float values when SMOTE is being performed as the process will generate synthetic values based on KNN algorithm.
+# For eg: We have to make sure that column values stay IN [1,0] and not something like 0.55 when synthetic values are being set up.
 
 X['gender'] = X['gender'].astype(np.int64)
 X['ever_married'] = X['ever_married'].astype(np.int64)
@@ -119,12 +127,11 @@ X['smoking_status'] = X['smoking_status'].astype(np.int64)
 X.info()
 
 
-#%%
-# install imbalanced-learn package that has SMOTE. 
+# %%
+# install imbalanced-learn package that has SMOTE.
 # pip install imbalanced-learn --user
-from imblearn.over_sampling import SMOTE
 
-smote = SMOTE(sampling_strategy = 'minority')
+smote = SMOTE(sampling_strategy='minority')
 X_sm, y_sm = smote.fit_resample(X, y)
 # %%
 
@@ -134,30 +141,28 @@ print(y_sm.value_counts())
 # 0    4699
 # Name: stroke, dtype: int64
 
-# We now have generated equal number of participants who have a stroke and participants who do not have a stroke. 
+# We now have generated equal number of participants who have a stroke and participants who do not have a stroke.
 
 # The data set is perfectly balanced now!
 
 # %%
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
 
-# Creating test/train data sets from the balanced set using sklearn train_test_split: 80% train, 20% test 
-X_train, X_test, y_train, y_test = train_test_split(X_sm, y_sm, test_size= 0.2, random_state= 15, stratify=y_sm)
+# Creating test/train data sets from the balanced set using sklearn train_test_split: 80% train, 20% test
+X_train, X_test, y_train, y_test = train_test_split(
+    X_sm, y_sm, test_size=0.2, random_state=15, stratify=y_sm)
 
-print(y_train.value_counts()) # 80% as training set   
-print(y_test.value_counts()) # 20% as test set
+print(y_train.value_counts())  # 80% as training set
+print(y_test.value_counts())  # 20% as test set
 
 # %%
-from sklearn.linear_model import LogisticRegression
 
 strokemodel = LogisticRegression(max_iter=1000)
 
 strokemodel.fit(X_train, y_train)
 
-#%%
+# %%
 strokemodel.score(X_test, y_test)
-# 0.8191489361702128 The model is nearly 82% effective in identifying or predicting the results of a participant getting a stroke or not. 
+# 0.8191489361702128 The model is nearly 82% effective in identifying or predicting the results of a participant getting a stroke or not.
 # %%
 # creating predicted target values using the model for X_test:
 y_predict = strokemodel.predict(X_test)
@@ -167,18 +172,18 @@ y_predict = strokemodel.predict(X_test)
 cm_stroke_model = confusion_matrix(y_test, y_predict)
 # array([[751, 189],
 #        [151, 789]], dtype=int64)
-#%%
+# %%
 
 # Creating a heatmap of the above confusion matrix for better visualization and understanding:
 
-sn.heatmap(cm_stroke_model, annot = True, fmt="d")
+sn.heatmap(cm_stroke_model, annot=True, fmt="d")
 plt.show()
 
 # y axis - truth values
 # x axis - predicted values
 
 # The heatmap of the confusion matrix shows that when the values in the y_test data set are compared with the predicted values from y_predict.
-# 751 participants were predicted to 'not have a stroke' and 751 time it was predicted right by the model. 189 times the participant 'did not have a stroke' but it was predicted they did. 151 participants were predidcted to 'have a stroke' but in reality they did not suffer from one. 789 times the participants were predicted to 'have a stroke' and 789 times they got one. 
+# 751 participants were predicted to 'not have a stroke' and 751 time it was predicted right by the model. 189 times the participant 'did not have a stroke' but it was predicted they did. 151 participants were predidcted to 'have a stroke' but in reality they did not suffer from one. 789 times the participants were predicted to 'have a stroke' and 789 times they got one.
 
 print(classification_report(y_test, y_predict))
 
@@ -189,5 +194,8 @@ print(classification_report(y_test, y_predict))
 #
 #    accuracy                           0.82      1880
 #   macro avg       0.82      0.82      0.82      1880
-#weighted avg       0.82      0.82      0.82      1880
-#%%
+# weighted avg       0.82      0.82      0.82      1880
+
+# Accuracy and f1-score of the model is 0.82 or 82% which is pretty decent given that the target variable has been modified.
+
+# %%
